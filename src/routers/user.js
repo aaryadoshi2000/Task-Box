0aa9215a3b8bd72691399b383255878b2212ebcd
aaryadoshi2000 <aaryadoshi2000@gmail.com>
2023-03-01 18:02:50 -0500

    env

--- src/routers/user.js
+++ src/routers/user.js
@@ -13,6 +13,7 @@ router.post('/users', async (req, res) => {
 		await user.save();
 		const token = await user.generateAuthToken();
 
+		console.log("processs .emvevev",process.env)
 		let transporter = nodemailer.createTransport({
 			service: 'gmail',
 			auth: {
